72528025240cb25494cdf0eff7d6d7c149204d24
eduaibar <eduaibar2006@gmail.com>
2025-11-15 17:18:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('generate').addEventListener('click', () => {
     const url = document.getElementById('url').value;
     const qrContainer = document.getElementById('qr');
-    qrContainer.innerHTML = ""; // Limpiar QR anterior
+    qrContainer.innerHTML = "";
     QRCode.toCanvas(url, { width: 200 }, function (error, canvas) {
         if (error) console.error(error);
         qrContainer.appendChild(canvas);
